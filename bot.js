f43df3fee34c9f99549d70f746fd01f4f32454a5
boertel <benjamin.oertel@gmail.com>
2016-03-09 21:31:44 -0800

    initial commit

---
+++ bot.js
@@ -0,0 +1,59 @@
+var RtmClient = require('@slack/client').RtmClient,
+    RTM_EVENTS = require('@slack/client').RTM_EVENTS,
+    RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM,
+    _ = require('lodash'),
+    foods = require('./foods');
+
+
+function findFoodDay() {
+    var now = new Date(),
+        day = now.getDate(),
+        month = now.getMonth() + 1;
+
+    day = day < 10 ? '0' + day : day;
+    month = month < 10 ? '0' + month : month;
+
+    var key = month + '-' + day;
+    return foods[key];
+}
+
+function createMessage(foodsList) {
+    var foodsOfDay = foodsList.map(function(food) {
+        return '*' + food + '*'
+    })
+    var message = 'Today is National ' + commasAnd(foodsOfDay) + ' Day. Bon appétit!';
+    return message;
+}
+
+function commasAnd(a) {
+    return [a.slice(0, -1).join(', '), a.slice(-1)[0]].join(a.length < 2 ? '' : ' and ');
+}
+
+
+// create a bot
+var toChannel = process.env.SLACK_TO_CHANNEL || 'general';
+var rtm = new RtmClient(process.env.SLACK_TOKEN, { logLevel: '' });
+rtm.start();
+
+var store = {
+    channels: {},
+    users: {}
+};
+
+
+rtm.on(RTM_CLIENT_EVENTS.AUTHENTICATED, function (payload) {
+    store = {
+        channels: payload.channels,
+        users: payload.users
+    }
+});
+
+
+rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function () {
+    var foodsOfDay = findFoodDay();
+    if (foodsOfDay  !== undefined) {
+        var message = createMessage(foodsOfDay);
+        var to = _.find(store.channels, { name: toChannel });
+        rtm.sendMessage(message, to.id)
+    }
+});
---
+++ foods.js
@@ -0,0 +1,719 @@
+module.exports = {
+	"01-02": [
+		"Creampuff"
+	],
+	"01-03": [
+		"Chocolate Covered Cherry"
+	],
+	"01-04": [
+		"Cheese Fondue"
+	],
+	"01-05": [
+		"Whipped Cream"
+	],
+	"01-09": [
+		"Cassoulet"
+	],
+	"01-19": [
+		"Popcorn"
+	],
+	"01-20": [
+		"Cheese"
+	],
+	"01-22": [
+		"Hot Sauce"
+	],
+	"01-23": [
+		"Pie"
+	],
+	"01-24": [
+		"Peanut Butter"
+	],
+	"01-27": [
+		"Chocolate Cake"
+	],
+	"02-02": [
+		"Tater Tot"
+	],
+	"00-NaN": [
+		"Ice Cream for Breakfast",
+		"Frozen Yogurt",
+		"Pizza Party",
+		"Doughnut",
+		"Ice Cream",
+		"Chili Dog",
+		"Mustard",
+		"Men Make Dinner",
+		"International Stout",
+		"Turkey  (informal name for Thanksgiving Day)",
+		"Sinkie"
+	],
+	"02-09": [
+		"Pizza"
+	],
+	"02-15": [
+		"I Want Butterscotch"
+	],
+	"02-22": [
+		"Margarita"
+	],
+	"02-23": [
+		"Banana Bread"
+	],
+	"02-29": [
+		"Frog Legs  (Celebration for Leap Day)"
+	],
+	"03-01": [
+		"Peanut Butter Lover's"
+	],
+	"03-02": [
+		"Banana Creme Pie"
+	],
+	"03-03": [
+		"Canadian Bacon"
+	],
+	"03-05": [
+		"Cheese Doodle"
+	],
+	"03-07": [
+		"Crown Roast of Pork"
+	],
+	"03-08": [
+		"Peanut Cluster"
+	],
+	"03-09": [
+		"Crabmeat",
+		"Meatball"
+	],
+	"03-10": [
+		"Blueberry Popover"
+	],
+	"03-11": [
+		"Oatmeal Nut Waffles",
+		"Johnny Appleseed"
+	],
+	"03-12": [
+		"Baked Scallops"
+	],
+	"03-13": [
+		"Coconut Torte"
+	],
+	"03-22": [
+		"Corndog"
+	],
+	"03-23": [
+		"Chips and Dip"
+	],
+	"03-27": [
+		"International Whiskey"
+	],
+	"03-31": [
+		"Clams on the Half Shell"
+	],
+	"04-02": [
+		"Peanut Butter and Jelly"
+	],
+	"04-05": [
+		"Caramel"
+	],
+	"04-07": [
+		"Beer"
+	],
+	"04-11": [
+		"Cheese Fondue"
+	],
+	"04-12": [
+		"Grilled Cheese Sandwich"
+	],
+	"04-15": [
+		"Banana"
+	],
+	"04-17": [
+		"Cheeseball"
+	],
+	"04-19": [
+		"Garlic",
+		"Rice Ball"
+	],
+	"04-24": [
+		"Pigs in a Blanket"
+	],
+	"04-25": [
+		"Zucchini Bread"
+	],
+	"04-26": [
+		"Pretzel"
+	],
+	"04-27": [
+		"Prime Rib"
+	],
+	"04-29": [
+		"Shrimp Scampi"
+	],
+	"05-04": [
+		"Candied Orange Peel"
+	],
+	"05-05": [
+		"Hoagie"
+	],
+	"05-11": [
+		"Eat What You Want"
+	],
+	"05-17": [
+		"Walnut"
+	],
+	"05-23": [
+		"Taffy"
+	],
+	"05-24": [
+		"Escargot"
+	],
+	"05-28": [
+		"Brisket",
+		"Burger"
+	],
+	"06-02": [
+		"Rotisserie Chicken",
+		"Rocky Road"
+	],
+	"06-03": [
+		"Chocolate Macaroon",
+		"Egg"
+	],
+	"06-04": [
+		"Cheese",
+		"Cognac"
+	],
+	"06-05": [
+		"Donut/Doughnut",
+		"Gingerbread",
+		"Moonshine"
+	],
+	"06-06": [
+		"Applesauce Cake"
+	],
+	"06-07": [
+		"Chocolate Ice Cream"
+	],
+	"06-10": [
+		"Iced Tea"
+	],
+	"06-11": [
+		"Corn on the cob day"
+	],
+	"06-12": [
+		"Peanut Butter Cookie"
+	],
+	"06-15": [
+		"Lobster"
+	],
+	"06-16": [
+		"Fudge"
+	],
+	"06-17": [
+		"Eat Your Vegetables"
+	],
+	"06-20": [
+		"Vanilla Milkshake",
+		"Ice Cream Soda",
+		"Kouign Amann"
+	],
+	"06-21": [
+		"Peaches ‘N’ Cream"
+	],
+	"06-25": [
+		"Catfish"
+	],
+	"06-26": [
+		"Chocolate Pudding"
+	],
+	"06-27": [
+		"Orange Blossom"
+	],
+	"06-28": [
+		"Ceviche",
+		"Tapioca"
+	],
+	"07-01": [
+		"Creative Ice Cream Flavor",
+		"Gingersnap"
+	],
+	"07-02": [
+		"Anisette"
+	],
+	"07-03": [
+		"Eat Beans",
+		"Chocolate Wafer"
+	],
+	"07-04": [
+		"Caesar Salad",
+		"Spareribs",
+		"Barbecue"
+	],
+	"07-05": [
+		"Apple Turnover"
+	],
+	"07-06": [
+		"Take Your Webmaster to Lunch",
+		"Fried Chicken"
+	],
+	"07-07": [
+		"Macaroni",
+		"Strawberry Sundae"
+	],
+	"07-08": [
+		"Chocolate with Almonds"
+	],
+	"07-09": [
+		"Sugar Cookie"
+	],
+	"07-10": [
+		"Piña colada"
+	],
+	"07-11": [
+		"Blueberry Muffin"
+	],
+	"07-12": [
+		"Pecan Pie"
+	],
+	"07-13": [
+		"French Fries",
+		"BBQ"
+	],
+	"07-14": [
+		"Grand Marnier",
+		"Mac & Cheese"
+	],
+	"07-15": [
+		"Tapioca Pudding",
+		"Gummi Worm"
+	],
+	"07-16": [
+		"Fresh Spinach",
+		"Corn Fritter"
+	],
+	"07-17": [
+		"Peach Ice Cream"
+	],
+	"07-18": [
+		"Caviar"
+	],
+	"07-19": [
+		"Daiquiri"
+	],
+	"07-20": [
+		"Fortune Cookie",
+		"Lollipop"
+	],
+	"07-21": [
+		"Junk Food"
+	],
+	"07-22": [
+		"Penuche Fudge"
+	],
+	"07-23": [
+		"Vanilla Ice Cream",
+		"Hot Dog"
+	],
+	"07-24": [
+		"Tequila",
+		"Drive-Thru"
+	],
+	"07-25": [
+		"Hot Fudge Sundae"
+	],
+	"07-26": [
+		"Coffee Milkshake"
+	],
+	"07-27": [
+		"Creme Brulee",
+		"Scotch"
+	],
+	"07-28": [
+		"Hamburger",
+		"Milk Chocolate"
+	],
+	"07-29": [
+		"Lasagna",
+		"Chicken Wing"
+	],
+	"07-30": [
+		"Cheesecake"
+	],
+	"07-31": [
+		"Raspberry Cake"
+	],
+	"08-03": [
+		"Watermelon",
+		"White Wine"
+	],
+	"08-10": [
+		"S'more"
+	],
+	"08-13": [
+		"Filet Mignon"
+	],
+	"08-14": [
+		"Creamsicle"
+	],
+	"08-15": [
+		"Lemon Meringue Pie"
+	],
+	"08-16": [
+		"Rum",
+		"World Bratwurst"
+	],
+	"08-17": [
+		"Vanilla Custard"
+	],
+	"08-18": [
+		"Pinot Noir"
+	],
+	"08-19": [
+		"Potato"
+	],
+	"08-21": [
+		"Spumoni"
+	],
+	"08-22": [
+		"Pecan Torte",
+		"Eat A Peach"
+	],
+	"08-23": [
+		"Sponge Cake"
+	],
+	"08-24": [
+		"Peach Pie",
+		"Waffle"
+	],
+	"08-25": [
+		"Banana Split",
+		"Whiskey Sour"
+	],
+	"08-26": [
+		"Cherry Popsicle"
+	],
+	"08-27": [
+		"Pots De Creme",
+		"Banana Lovers"
+	],
+	"08-28": [
+		"Cherry Turnover",
+		"Crackers Over The Keyboard"
+	],
+	"08-29": [
+		"More Herbs, Less Salt",
+		"Lemon Juice",
+		"Chop Suey"
+	],
+	"08-30": [
+		"Toasted Marshmallow"
+	],
+	"08-31": [
+		"Trail Mix"
+	],
+	"09-01": [
+		"Cherry Popover"
+	],
+	"09-02": [
+		"Blueberry Popsicle"
+	],
+	"09-03": [
+		"Welsh Rarebit"
+	],
+	"09-04": [
+		"Macademia Nut"
+	],
+	"09-05": [
+		"Cheese Pizza"
+	],
+	"09-06": [
+		"Coffee Ice Cream"
+	],
+	"09-07": [
+		"Beer Lover's",
+		"Acorn Squash",
+		"Salami"
+	],
+	"09-09": [
+		"Wienerschnitzel",
+		"Steak Au Poivre"
+	],
+	"09-10": [
+		"TV Dinner"
+	],
+	"09-11": [
+		"Hot Cross Bun"
+	],
+	"09-12": [
+		"Chocolate Milkshake"
+	],
+	"09-13": [
+		"Peanut",
+		"International Chocolate"
+	],
+	"09-14": [
+		"Eat a Hoagie",
+		"Cream Filled Donut"
+	],
+	"09-15": [
+		"Crème de Menthe",
+		"Double Cheeseburger",
+		"Linguine",
+		"Cheese Toast"
+	],
+	"09-16": [
+		"Guacamole",
+		"Cinnamon-Raisin Bread"
+	],
+	"09-17": [
+		"Apple Dumpling",
+		"Monte Cristo sandwich"
+	],
+	"09-18": [
+		"Cheeseburger"
+	],
+	"09-19": [
+		"Butterscotch Pudding"
+	],
+	"09-20": [
+		"Pepperoni Pizza",
+		"Punch",
+		"Rum Punch"
+	],
+	"09-21": [
+		"Pecan Cookie"
+	],
+	"09-25": [
+		"Lobster"
+	],
+	"09-26": [
+		"Better Breakfast",
+		"Pancake",
+		"Dumpling  National Chocolate Milk Day",
+		"Johnny Appleseed"
+	],
+	"09-27": [
+		"Chocolate Milk",
+		"Corned Beef Hash"
+	],
+	"09-28": [
+		"Family  – A Day to Eat Dinner with Your Children",
+		"Strawberry Cream Pie",
+		"Drink Beer"
+	],
+	"09-29": [
+		"Coffee",
+		"Biscotti"
+	],
+	"09-30": [
+		"Hot Mulled Cider"
+	],
+	"10-04": [
+		"Vodka",
+		"Taco"
+	],
+	"10-06": [
+		"Noodle"
+	],
+	"10-11": [
+		"Sausage Pizza"
+	],
+	"10-14": [
+		"Dessert"
+	],
+	"10-18": [
+		"Chocolate Cupcake"
+	],
+	"10-20": [
+		"Brandied Fruit"
+	],
+	"10-23": [
+		"Boston Creme Pie"
+	],
+	"10-24": [
+		"Bologna",
+		"Food"
+	],
+	"10-25": [
+		"Greasy Food"
+	],
+	"10-28": [
+		"Chocolate day"
+	],
+	"10-29": [
+		"Oatmeal"
+	],
+	"10-30": [
+		"Haunted Refrigerator Night",
+		"Candy Corn"
+	],
+	"10-31": [
+		"Candy Apple",
+		"Caramel Apple"
+	],
+	"11-01": [
+		"Vinegar",
+		"French Fried Clam",
+		"Cook For Your Pets"
+	],
+	"11-02": [
+		"Deviled Egg"
+	],
+	"11-03": [
+		"Sandwich"
+	],
+	"11-04": [
+		"Candy",
+		"Eating Healthy"
+	],
+	"11-05": [
+		"Doughnut"
+	],
+	"11-06": [
+		"Nachos"
+	],
+	"11-07": [
+		"Bittersweet Chocolate with Almonds"
+	],
+	"11-08": [
+		"Harvey Wallbanger",
+		"Cook Something Bold and Pungent",
+		"Cappuccino",
+		"Shot"
+	],
+	"11-09": [
+		"Scrapple"
+	],
+	"11-10": [
+		"Vanilla Cupcake"
+	],
+	"11-11": [
+		"Sundae"
+	],
+	"11-12": [
+		"Pizza with the Works Except Anchovies",
+		"Chicken Soup for the Soul",
+		"Happy Hour"
+	],
+	"11-13": [
+		"Indian Pudding"
+	],
+	"11-14": [
+		"Pickle",
+		"Spicy Guacamole"
+	],
+	"11-15": [
+		"Spicy Hermit Cookie",
+		"Clean Out Your Refrigerator"
+	],
+	"11-16": [
+		"Fast Food"
+	],
+	"11-17": [
+		"Baklava",
+		"Homemade Bread"
+	],
+	"11-18": [
+		"Vichyssoise",
+		"Apple Cider"
+	],
+	"11-19": [
+		"Carbonated Beverage with Caffeine"
+	],
+	"11-20": [
+		"Peanut Butter Fudge"
+	],
+	"11-21": [
+		"Stuffing"
+	],
+	"11-22": [
+		"Cranberry Relish"
+	],
+	"11-23": [
+		"Cashew",
+		"Eat A Cranberry"
+	],
+	"11-24": [
+		"Espresso"
+	],
+	"11-25": [
+		"Parfait"
+	],
+	"11-26": [
+		"Cake"
+	],
+	"11-27": [
+		"Bavarian Cream Pie"
+	],
+	"11-28": [
+		"French Toast"
+	],
+	"11-29": [
+		"Throw Out Your Leftovers",
+		"Lemon Cream Pie",
+		"Chocolates"
+	],
+	"11-30": [
+		"Mousse"
+	],
+	"12-01": [
+		"Eat a Red Apple",
+		"Pie"
+	],
+	"12-02": [
+		"Fritters"
+	],
+	"12-04": [
+		"Cookie"
+	],
+	"12-05": [
+		"Rock Candy"
+	],
+	"12-08": [
+		"Brownie"
+	],
+	"12-10": [
+		"Lager"
+	],
+	"12-11": [
+		"Noodle Ring",
+		"Have A Bagel"
+	],
+	"12-13": [
+		"Ice Cream"
+	],
+	"12-15": [
+		"Cupcake"
+	],
+	"12-16": [
+		"Chocolate-Covered Anything"
+	],
+	"12-17": [
+		"Maple Syrup"
+	],
+	"12-20": [
+		"Sangria"
+	],
+	"12-21": [
+		"Hamburger"
+	],
+	"12-22": [
+		"Date Nut Bread"
+	],
+	"12-24": [
+		"Eggnog"
+	],
+	"12-25": [
+		"Pumpkin Pie"
+	],
+	"12-27": [
+		"Fruitcake"
+	],
+	"12-30": [
+		"Bicarbonate of Soda"
+	],
+	"12-31": [
+		"Champagne"
+	]
+};
