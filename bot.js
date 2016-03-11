ea7dc5269fdc5fa48686894f69d75e72c3fce979
boertel <benjamin.oertel@gmail.com>
2016-03-11 11:58:05 -0800

    keep server alive

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
-console.log('started');
+var http = require('http');
+
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Ok, dyno is awake.');
+}).listen(process.env.PORT || 5000);
