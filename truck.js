5699147b0973146a47304043469356a6e1391e7d
boertel <benjamin.oertel@gmail.com>
2017-04-10 15:35:18 -0700

    works with new infrastruckture api

--- truck.js
+++ truck.js
@@ -1,37 +1,41 @@
 var https = require('https');
-var url = require('url');
+var URL = require('url').URL;
 
 
 var INFRASTRUCKTURE_URL = 'https://www.infrastruckture.com/public-api/events';
 
+function generateYelpSearch(name) {
+    var name = encodeURIComponent(name);
+    return 'https://www.yelp.com/search?find_desc=' + name;
+}
+
 function get(url, callback) {
-    var parseURL = url.parse(url);
+    var parseURL = new URL(url);
+    var path = parseURL.pathname + parseURL.search;
     var options = {
         host: parseURL.host,
-        path: parseURL.path,
+        path: path,
         protocol: parseURL.protocol,
         method: 'GET',
         headers: {
             'Authorization': 'Bearer ' + process.env.INFRASTRUCKTURE_TOKEN,
         },
     };
-    
-    console.log(options);
-    
-    var promise = new Promise(function (resolve) {
-        https.get(options, function(response) {
-            var body = '';
-            response.on('data', function(d) {
-                body += d;
-            });
 
-            response.on('end', function() {
-                var parsed = JSON.parse(body);
-                resolve(parsed);
+    var promise = new Promise(function (resolve, reject) {
+            https.get(options, function(response) {
+                var body = '';
+                response.on('data', function(d) {
+                    body += d;
+                });
+
+                response.on('end', function() {
+                    var parsed = JSON.parse(body);
+                    resolve(parsed);
+                });
             });
-        });
     });
-    
+
     return promise;
 }
 
@@ -43,19 +47,21 @@ function today() {
 
     day = day < 10 ? '0' + day : day;
     month = month < 10 ? '0' + month : month;
-    return [year, month, day].join('-'); 
+    return [year, month, day].join('-');
 }
 
 function getEvents() {
-    var to_ = (new Date().getTime()),
-        from_ = to_ + (1000 * 60 * 60 * 24 * 7),
+    var from_ = (new Date().getTime()),
+        to_ = from_ + (1000 * 60 * 60 * 24 * 7),
         locationId = process.env.LOCATION_ID;
-    return get(INFRASTRUCKTURE_URL + '?dateTo=' + to_ + '&locationId=' + locationId + '&dateFrom=' + from_).then(function(response) {
+    return get(INFRASTRUCKTURE_URL + '?dateFrom=' + from_ + '&locationId=' + locationId + '&dateTo=' + to_).then(function(response) {
         var events = response.data.events;
         var firstEvent = events[0];
-        
+
         if (firstEvent.startTime.indexOf(today()) !== -1) {
             return firstEvent;
+        } else {
+            throw new Error('no events found.')
         }
     });
 }
@@ -70,13 +76,8 @@ function getVendors(evt) {
     });
 }
 
-function generateYelpSearch(name) {
-    var name = encodeURIComponent(name);
-    return 'https://www.yelp.com/search?find_desc=' + name;
-}
-
 function getMessage() {
-    getEvents()
+    return getEvents()
         .then(getVendors)
         .then(function(vendors) {
             var message;
@@ -88,6 +89,9 @@ function getMessage() {
                 message = output.join('\n');
             }
             return message;
+        })
+        .catch(function(error) {
+            console.log(error.message);
         });
 
 }
