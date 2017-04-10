f151cf3bd97c8eafc1cfb7e0310397e256908575
Benjamin Oertel <benjamin.oertel@gmail.com>
2017-04-10 15:07:27 -0700

    Update truck.js

--- truck.js
+++ truck.js
@@ -1,42 +1,71 @@
 var https = require('https');
+var url = require('url');
+
+
+var INFRASTRUCKTURE_URL = 'https://www.infrastruckture.com/public-api/events';
 
 function get(url, callback) {
-    https.get(process.env.OFFTHEGRID_URL, function(response) {
-        var body = '';
-        response.on('data', function(d) {
-            body += d;
-        });
+    var parseURL = url.parse(url);
+    var options = {
+        host: parseURL.host,
+        path: parseURL.path,
+        protocol: parseURL.protocol,
+        method: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + process.env.INFRASTRUCKTURE_TOKEN,
+        },
+    };
+    
+    console.log(options);
+    
+    var promise = new Promise(function (resolve) {
+        https.get(options, function(response) {
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
 
-        response.on('end', function() {
-            var parsed = JSON.parse(body);
-            callback && callback(parsed);
+            response.on('end', function() {
+                var parsed = JSON.parse(body);
+                resolve(parsed);
+            });
         });
+    });
+    
+    return promise;
+}
+
+function today() {
+    var now = new Date(),
+        year = now.getFullYear(),
+        day = now.getDate(),
+        month = now.getMonth() + 1;
+
+    day = day < 10 ? '0' + day : day;
+    month = month < 10 ? '0' + month : month;
+    return [year, month, day].join('-'); 
+}
+
+function getEvents() {
+    var to_ = (new Date().getTime()),
+        from_ = to_ + (1000 * 60 * 60 * 24 * 7),
+        locationId = process.env.LOCATION_ID;
+    return get(INFRASTRUCKTURE_URL + '?dateTo=' + to_ + '&locationId=' + locationId + '&dateFrom=' + from_).then(function(response) {
+        var events = response.data.events;
+        var firstEvent = events[0];
+        
+        if (firstEvent.startTime.indexOf(today()) !== -1) {
+            return firstEvent;
+        }
     });
 }
 
-function getVendors(callback) {
-    get(process.OFFTHEGRID_URL, function(response) {
-        var events = response.MarketDetail.Events;
-        if (events.length) {
-            var event = events[0].Event;
-            var vendors = [];
-
-            var date = event.year + '-' + (event.month_day.replace('.', '-'));
-
-            var now = new Date(),
-                year = now.getFullYear(),
-                day = now.getDate(),
-                month = now.getMonth() + 1;
-
-            day = day < 10 ? '0' + day : day;
-            month = month < 10 ? '0' + month : month;
-
-            var key = [year, month, day].join('-');
-
-            if (date === key) {
-                vendors = events[0].Vendors;
-            }
-            callback && callback(vendors);
+function getVendors(evt) {
+    return get(INFRASTRUCKTURE_URL + '/' + evt.id).then(function(response) {
+        var vendors = [];
+        vendors = response.data.event.services;
+        if (vendors.length) {
+            return vendors;
         }
     });
 }
@@ -46,19 +75,20 @@ function generateYelpSearch(name) {
     return 'https://www.yelp.com/search?find_desc=' + name;
 }
 
-function getMessage(callback) {
-    getVendors(function(vendors) {
-        var message;
-        if (vendors.length) {
-            var output = ['Today\'s food trucks are:'];
-            vendors.forEach(function(vendor) {
-                var website = vendor.website || generateYelpSearch(vendor.name);
-                output.push('- _' + vendor.name + '_ (' + vendor.cuisine + ') ' + website);
-            });
-            message = output.join('\n');
-        }
-        callback && callback(message);
-    });
+function getMessage() {
+    getEvents()
+        .then(getVendors)
+        .then(function(vendors) {
+            var message;
+            if (vendors.length) {
+                var output = ['Today\'s food trucks are:'];
+                vendors.forEach(function(vendor) {
+                    output.push('- _' + vendor.name + '_');
+                });
+                message = output.join('\n');
+            }
+            return message;
+        });
 
 }
 
